7051820e89fd9c7b25a47a0ad1f8c0f60e70b68a
Jacqueline-Tsai <jaynnah01180118@gmail.com>
2021-07-30 02:34:31 +0800

    add main

--- Main.cpp
+++ Main.cpp
@@ -5,12 +5,18 @@
 using namespace std;
 
 int main(){
-    //srand(time(NULL));
-    //SA sa;
-    //LNS lns(5, 1, 0.9, 1, 1, 1);
-    //lns.input();
-    //lns.solve();
-    //lns.output();
+    srand(time(NULL));
+	
+	LNS lns(5, 1, 0.9, 1, 1, 1);
+
+	// for test
+	vector<vector<int>> list = { {6, -1, 5, 7, -1, 4, 2}, { 3, 2, 5, -1, 6, -1, 7}, { 2, -1, 4, 7, -1 } };
+	lns.calculateObjective(list);
+
+
+    lns.input();
+    lns.solve();
+    lns.output();
 	
 	//cout << "bold red text" << std::endl;
 	//cout << "\033[1;31mbold red text\033[0m\n";
